104614a66cc7c2aa7c6979d8869d568f25bda655
GladyshAleksandr <93590594+GladyshAleksandr@users.noreply.github.com>
2022-02-13 16:15:46 +0200

    Add files via upload

---
+++ src/common/preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import loadingIcon from "../../assets/images/loadingIcon.jpg";
+
+
+let Preloader: React.FC = (props) =>  {
+
+return <div><img src={loadingIcon}/> 
+</div>
+}
+
+export default Preloader;
